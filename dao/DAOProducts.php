7c3fced0ff6aa47e0c9f54bb88f1934c532a8a12
gitSushi <lossushi37@gmail.com>
2021-06-22 08:44:31 +0000

    dynamic nav menu

---
+++ dao/DAOProducts.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BWB\Framework\mvc\dao;
+
+use BWB\Framework\mvc\DAO;
+use PDO;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DAODefault
+ *
+ * @author loic
+ */
+class DAOProducts extends DAO
+{
+    //put your code here
+    public function create($array)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+
+    public function getAll()
+    {
+        return $this->getPdo()->query("SELECT * FROM product")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllBy($filter)
+    {
+    }
+
+    public function retrieve($id)
+    {
+    }
+
+    public function update($array)
+    {
+    }
+}
--- views/testGroupView.php
+++ views/testGroupView.php
@@ -13,34 +13,10 @@
 <body>
     <main class="container mt-4">
 
-        <!-- Main container -->
-        <nav class="level">
-            <!-- Left side -->
-            <div class="level-left">
-                <div class="level-item">
-                    <p class="subtitle is-3">
-                        <span class="icon">
-                            <i class="fas fa-arrow-left"></i>
-                        </span>
-                    </p>
-                </div>
-                <div class="level-item">
-                    <p class="title">Création d'un groupe de tests</p>
-                </div>
-            </div>
-
-            <!-- Right side -->
-            <div class="level-right">
-                <div class="level-item">
-                    <p class="subtitle is-3">
-                        <span class="icon">
-                            <i class="fas fa-user-cog"></i>
-                        </span>
-                    </p>
-                </div>
-                <p class="level-item"><strong>serial #</strong></p>
-            </div>
-        </nav>
+        <?php
+        $navTitle = "Création d'un groupe de tests";
+        include("views/nav.php");
+        ?>
 
         <nav class="panel pb-4">
             <p class="panel-heading">
