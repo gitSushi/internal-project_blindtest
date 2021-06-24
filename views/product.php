847c1e01a0d19b6bfb07e367d95a9dc6f4a0cd95
Yohan Beneito <bwb.beneito.y@fondespierre.com>
2021-06-24 11:34:14 +0000

    ok

--- views/product.php
+++ views/product.php
@@ -8,28 +8,43 @@
     <nav class="panel">
         <div class="panel-heading">
             <label for="productList">Entrez un nom de produit:</label>
-            <input list="products" id="productList" name="productList" placeholder="nom d'un produit" required/>
-            <datalist id="products">
+            <input list="dl" id="text" name="text" placeholder="nom d'un produit" required/>
+            <datalist id="dl">
                 <?php foreach ($products as $prod) : ?>
-                    <option value="<?= $prod['name'] ?>"></option>
+                    <option id="<?= $prod['id']?>" value="<?= $prod['name'] ?>"></option>
                 <?php endforeach ?>
             </datalist>
             <button type="submit" id="btn" class="button is-primary is-small">Choose</button> 
         </div>
         <div class="panel-block">
-            <p id="test"></p>
+            <p id="msg"></p>
+            <p id="display"></p>   
         </div>
     </nav>
     <!--Tests JS-->
     <script>
-        // Au click du bouton le produit selectionné est entrée dans la variable selected
+        // Au click du bouton l'id du produit est affiché en dessous
             var btn = document.getElementById("btn")
-            var test = document.getElementById('test')
-            btn.addEventListener("click", function() {
-                var selected = document.getElementById("productList").value
-                test.innerHTML = selected
-                
-            })
+
+            const text = document.getElementById('text')
+            const dl = document.getElementById('dl')
+            const msg = document.getElementById('msg')
+           
+            btn.addEventListener('click', () => {
+                const index = [... dl.options].map(o => o.value).indexOf(text.value)
+                    if (index === - 1) {
+                        msg.innerHTML = "Aucun id à afficher."
+                    } else {
+                        msg.innerHTML = "Id = " + dl.options[index].id
+                    }
+            }, false)
+        //end récup prodId
+
+        //Affichage des tests correspondants
+      
+
+
+        //end affiche    
     </script>
 </body>
 </html>
