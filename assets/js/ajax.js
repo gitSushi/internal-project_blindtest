17d4137206846c70534c0ab6b40417af1ce0267e
Yohan Beneito <bwb.beneito.y@fondespierre.com>
2021-06-25 14:09:22 +0000

    datas ok

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -9,32 +9,50 @@ btn.addEventListener("click", (e) => {
     if (index === - 1) {
         msg.innerHTML = "Aucun id à afficher."
     } else {
-        msg.innerHTML = dl.options[index].id
         var idasked = dl.options[index].id
     }
    requete(idasked)
 })
 
 function requete(idasked){
+    
      //requete
      var xhr = new XMLHttpRequest()
+     xhr.open("post","http://localhost:1597/products/get", true)
+     xhr.responseType = "json"
      xhr.onreadystatechange = function(){
          if(this.readyState == 4 && this.status == 200){
-             // msg.innerHTML += this.responseText 
-             var datas = this.response.users
-            
-                 msg.innerHTML += "Bonjour je suis " + datas[idasked].nom + ". "
-             
-             console.log(this.response);
+                renderGr(this.response)
          }else if(this.readyState == 4 && this.response == 404){
              alert("404")
          }
-     }
-     xhr.open("GET","/assets/js/text.txt", true)
-     xhr.responseType = "json"
-     xhr.send()
-     console.log(idasked);
+
+     }     
+     xhr.send(JSON.stringify({idasked}));
 }
 
-
+function renderGr(response) {
+    console.log(response);
+    var groupes = JSON.stringify(response);
+    var grobj = JSON.parse(groupes);
    
+    grobj.forEach(element => {
+        var box = document.createElement("div");
+        msg.appendChild(box);
+        var name = document.createElement("p");
+        name.innerHTML = element.name;
+        box.appendChild(name)
+        var serial = document.createElement("p")
+        serial.innerHTML = element.serial_number
+        box.appendChild(serial)
+        var tgname = document.createElement("p")
+        tgname.innerHTML = element.tgname
+        box.appendChild(tgname)
+        var passed = document.createElement("p")
+        passed.innerHTML = element.has_product_passed_test
+        box.appendChild(passed)
+    });
+
+    
+
+}
